460ced0774a6bf17bbe88709334a04d3ed7684f4
Arseni Pantsialei <arsenipantsialei@gmail.com>
2020-10-16 10:44:57 +0300

    feat: solve the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
+  let arr = n.toString().split('');
   
+   if (arr[0] === '-') arr = arr.slice(1);
+   if (arr[arr.length - 1] === '0') arr = arr.slice(0, arr.length - 1);
+
+   return arr.reverse().join('');
 }
